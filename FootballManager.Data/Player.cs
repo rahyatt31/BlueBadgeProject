58bb2fad1183c97a1ad478f2a71754f8d7b548fc
rahyatt <rahyatt31@gmail.com>
2020-06-08 13:04:13 -0400

    PlayerStats

--- FootballManager.Data/Player.cs
+++ FootballManager.Data/Player.cs
@@ -17,13 +17,9 @@ namespace FootballManager.Data
         public string PlayerFirstName { get; set; }
         [Required]
         public string PlayerLastName { get; set; }
-
-        //[MaxLength(2, ErrorMessage = "Please enter the abbriviation with consisting of only 2 characters. (QB, RB, WR, TE, OL, DL, LB, CB, FS, SS)")]
         [Required]
         public string PlayerPosition { get; set; }
-
         [Required]
-        //[MaxLength(2, ErrorMessage = "Please enter the abbriviation with consisting of only 2 characters. (01-99)")]
         public int PlayerJerseyNumber { get; set; }
         [Required]
         public double PlayerHeightByInches { get; set; }
@@ -33,5 +29,7 @@ namespace FootballManager.Data
         [ForeignKey("Team")]
         public int TeamID { get; set; }
         public virtual Team Team { get; set; }
+
+        public virtual ICollection<PlayerStat> PlayerStats { get; set; }
     }
 }
